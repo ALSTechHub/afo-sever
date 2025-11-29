1371aff7a9f70ec923b061e84f11472bf2f47afa
ALSTechHub <alstechhub@gmail.com>
2025-11-29 17:57:48 +0300

    Update transaction.routes.js

--- route/transaction.routes.js
+++ route/transaction.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { adminRoute, protectRoute } from '../middleware/authMiddleware.js';
+import { adminRoute, protectRoute } from '../middleware/authMiddleWare.js';
 import { getAllTransactions } from '../controller/transaction.controller.js';
 
 const router = express.Router();
 
 router.get('/', protectRoute,adminRoute, getAllTransactions);
+
 
 export default router;
